884945fe6870f8f526054b9388a9a2a9518d31cf
Pragya28 <pragyajha2812@gmail.com>
2026-01-01 22:07:48 +0530

    basic config and icon changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
+const pwaManifest: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  manifest: {
+    name: "Trace",
+    short_name: "trace",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+  },
+};
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(pwaManifest)],
+});
